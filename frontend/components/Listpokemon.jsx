a3545720d454ca4b225eb5ea1a6bacd9ee894118
Nabil Mahel <nabilmhl-dev@outlook.com>
2024-09-12 14:40:22 +0200

    recuperationdesnoms

--- frontend/components/Listpokemon.jsx
+++ frontend/components/Listpokemon.jsx
@@ -1,7 +1,27 @@
+import { useEffect, useState } from "react";
 
+const ListPokemon = () => {
+    const [pokemons, setPokemons] = useState([]);
 
-export default function listPokemon() {
-  return (
-    <div>listPokemon</div>
-  )
-}
+    useEffect(() => {
+        fetch('https://pokeapi.co/api/v2/pokemon?limit=20')
+        .then(response => response.json())
+        .then(data => {
+            setPokemons(data.results);
+        })
+        .catch(error => console.error('Erreur lors de la récupération des données', error));
+    }, []);
+
+    return (
+        <div>
+            {pokemons.map(pokemon => (
+                <div key={pokemon.name}>
+                    <h3>{pokemon.name}</h3>
+                
+                </div>
+            ))}
+        </div>
+    );
+};
+
+export default ListPokemon;
