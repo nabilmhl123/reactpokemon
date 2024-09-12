b88d9b270b859f76cbd2d8ce1c6cc0207d4ea8f6
Nabil Mahel <nabilmhl-dev@outlook.com>
2024-09-12 14:44:49 +0200

    lesnomsetimageaveclecss

--- frontend/components/Listpokemon.jsx
+++ frontend/components/Listpokemon.jsx
@@ -5,19 +5,28 @@ const ListPokemon = () => {
 
     useEffect(() => {
         fetch('https://pokeapi.co/api/v2/pokemon?limit=20')
-        .then(response => response.json())
-        .then(data => {
-            setPokemons(data.results);
-        })
-        .catch(error => console.error('Erreur lors de la récupération des données', error));
+            .then(response => response.json())
+            .then(data => {
+                // Obtenez les détails de chaque Pokémon pour les images
+                const pokemonDetailsPromises = data.results.map(pokemon =>
+                    fetch(pokemon.url).then(response => response.json())
+                );
+
+                Promise.all(pokemonDetailsPromises)
+                    .then(pokemonDetails => {
+                        setPokemons(pokemonDetails);
+                    })
+                    .catch(error => console.error('Erreur lors de la récupération des détails des Pokémon', error));
+            })
+            .catch(error => console.error('Erreur lors de la récupération des données', error));
     }, []);
 
     return (
-        <div>
+        <div className="pokemon-container">
             {pokemons.map(pokemon => (
-                <div key={pokemon.name}>
-                    <h3>{pokemon.name}</h3>
-                
+                <div key={pokemon.name} className="pokemon-card">
+                    <h3 className="pokemon-name">{pokemon.name}</h3>
+                    <img src={pokemon.sprites.front_default} alt={pokemon.name} className="pokemon-image" />
                 </div>
             ))}
         </div>
